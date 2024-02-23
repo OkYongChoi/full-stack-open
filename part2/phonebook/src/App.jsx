b9eb74773886cebdf155b19d5eb9ea8b5eea9670
Richard Choi <richokychoi@gmail.com>
2024-02-23 18:38:33 +0900

    Exercises 2.12 (The Phonebook Step7) Done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -70,8 +70,12 @@ const App = () => {
         name: newName,
         number: newNumber,
       }
-      setPersons(persons.concat(personObject))
-      setNewName('')
+      axios.post('http://localhost:3001/persons', personObject)
+      .then( response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+      })
+     
     }
   }
 
