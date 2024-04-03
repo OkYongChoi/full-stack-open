bd8b43b3737e253c24699aa4816ca40c1180eadd
Okyong Choi <okyongchoi@Okyongs-MacBook-Air.local>
2024-04-04 01:15:20 +0900

    Exercises 3.4 (Phonebook backend step 4) Done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -43,6 +43,13 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.json(person)
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter((p) => p.id !== id)
+
+  response.status(204).end()
 })
 
 app.get('/info', (request, response) => {
