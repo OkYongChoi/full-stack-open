c30dfecf98ccb0037d497194c2a781e3691ad769
Richard Choi <richokychoi@gmail.com>
2024-08-14 13:36:44 +0900

    Exercises 3.15 & 3.16 (Full Stack Phonebook) Done)

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -39,18 +39,18 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(response.params.id)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
     .then(person => {
       response.json(person)
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((p) => p.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result=> {response.status(204).end()})
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -82,6 +82,21 @@ app.post('/api/persons',(request, response) => {
     response.json(savedPerson)
   )
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name ==='CastError') {
+    return response.status(400).send({error:'malformtted id'})
+  }
+  next(error)
+}
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
