f4bb20e33f1ede070a486e4392ba3ef24f85eafc
Okyong <richokychoi@gmail.com>
2024-12-28 22:48:31 +0900

    Exercises 3.22 (Lint Configuration) Done

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
+if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
@@ -8,7 +8,7 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url = `mongodb+srv://ok9623:${password}@cluster0.qti7pv8.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
 
@@ -19,31 +19,28 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-if (process.argv.length == 5) {
+if (process.argv.length === 5) {
   const person = new Person({
     name: process.argv[3],
-    phone: process.argv[4]
+    phone: process.argv[4],
   })
-  
-  person.save().then(result => {
-    console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+  person.save().then(() => {
+    console.log(
+      `added ${process.argv[3]} number ${process.argv[4]} to phonebook`,
+    )
     mongoose.connection.close()
   })
-} else if (process.argv.length == 4) {
-  Person
-  .find({name: process.argv[3]})
-  .then(result => {
-    result.forEach(person => {
+} else if (process.argv.length === 4) {
+  Person.find({ name: process.argv[3] }).then((result) => {
+    result.forEach((person) => {
       console.log(person)
     })
     mongoose.connection.close()
   })
 } else {
   console.log('Phonebook: ')
-  Person
-  .find()
-  .then(result => {
-    result.forEach(person => {
+  Person.find().then((result) => {
+    result.forEach((person) => {
       console.log(person)
     })
     mongoose.connection.close()
