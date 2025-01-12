b62e6e7c9e7a2e0705bfa9cdccab04a70f460f21
Okyong <richokychoi@gmail.com>
2025-01-12 23:12:22 +0900

    Exercises 4.5 (Helper Functions and Unit Tests, step 3) Done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,7 +2,18 @@ const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs.reduce((acc, blog) => acc + blog.likes, 0)
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const favorite = blogs.reduce((acc, blog) =>
+    acc.likes > blog.likes ? acc : blog,
+  )
+
+  return favorite
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
